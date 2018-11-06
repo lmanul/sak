2ba47a65dead9a65167f7e89b1bd90850d63ebe2
Manu Cornet <m@ma.nu>
2018-11-06 13:43:49 +0000

    Sanitize file name: de-dupe underscores

--- util.py
+++ util.py
@@ -68,6 +68,7 @@ def sanitize_for_filename(input):
     output = output[1:]
   output = output.replace("._", "_")
   output = output.replace("_.", ".")
+  output = output.replace("__", "_")
   return output
 
 def get_date_prefix():
