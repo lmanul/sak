37cda44b42286c97e687b0c147451b6c451e8fbe
Manu Cornet <m@ma.nu>
2020-07-13 00:23:11 +0000

    Start adding a simple timezone conversion script

---
+++ timezones.py
@@ -0,0 +1,13 @@
+TIMEZONES = {
+    "CET": "Europe/Paris",
+    "EST": "US/Eastern",
+    "PST": "US/Pacific",
+    "UTC": "UTC",
+}
+
+SHORTS = {
+    "C": "CET",
+    "E": "EST",
+    "P": "PST",
+    "U": "UTC",
+}
