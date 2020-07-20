a7eed1a7a0b0942d0b25a9f25b48da5b05b1df56
Manu Cornet <m@ma.nu>
2020-07-20 00:37:31 +0000

    Add a few more time zones

--- timezones.py
+++ timezones.py
@@ -1,6 +1,10 @@
 TIMEZONES = {
     "CET": "Europe/Paris",
+    "CST": "Asia/Shanghai",
+    "EET": "Europe/Tallinn",
     "EST": "US/Eastern",
+    "ICT": "Asia/Bangkok",
+    "JST": "Asia/Tokyo",
     "PST": "US/Pacific",
     "UTC": "UTC",
 }
