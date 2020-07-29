debe2a4e8f5040d959ad2b0c79421d09c84f37a3
Manu Cornet <m@ma.nu>
2020-07-29 07:44:48 +0000

    A little more robustness in handling more full calendars

--- cal.py
+++ cal.py
@@ -2,6 +2,14 @@ import datetime
 import shlex
 import subprocess
 
+debug = False
+
+EVENT_TITLES_TO_IGNORE = [
+    "If you schedule here, I will likely miss it",
+    "No meeting Thursday",
+    "No-meeting Thursdays",
+]
+
 # Assumes the two arguments are sorted (first element of e is lower than
 # first element of f). This also assumes the second element of each even is
 # greater than the second.
@@ -42,21 +50,31 @@ def get_events_for_next_n_days(days):
            "--format-event='" + event_format + "' "
            "--format-recur-apt='" + event_format + "' "
            "--format-recur-event='" + event_format + "'")
+    if debug:
+        print(cmd)
     current_date_from_output = None
     for l in subprocess.check_output(shlex.split(cmd)).decode().split("\n"):
         l = l.strip()
         if l == "":
             continue
-        if l.count("/") == 2:
+        if l.count("/") == 2 and len(l) == len("12/34/56:"):
             # Date
             if l.endswith(":"):
                 l = l[:-1]
             p = l.split("/")
             current_date_from_output = datetime.datetime(
                 2000 + int(p[2]), int(p[0]), int(p[1]))
+            added_events_for_today = set()
             continue
 
         (start, end, title) = l.split("|", 2)
+        should_add = True
+        for e in EVENT_TITLES_TO_IGNORE:
+            if e in title:
+                should_add = False
+                break
+        if not should_add:
+            continue
         if start == "?":
             day_start = current_date_from_output.replace(hour=0, minute=0)
             start = day_start.timestamp()
@@ -65,5 +83,10 @@ def get_events_for_next_n_days(days):
             end = day_end.timestamp()
         start = int(start)
         end = int(end)
+        event_fp = str(start) + "|" + str(end) + "|" + str(title)
+        if event_fp in added_events_for_today:
+            # Don't add duplicates
+            continue
         out.append([start, end, title])
+        added_events_for_today.add(event_fp)
     return out
