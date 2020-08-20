3eef5433d9046b86e048663b5089ffcf19cc1e3f
Manu Cornet <m@ma.nu>
2020-08-20 09:19:23 +0000

    Iterate over layers

--- gimp_scripts/gimp_extract_layers.scm
+++ gimp_scripts/gimp_extract_layers.scm
@@ -1,14 +1,30 @@
 (define (gimp-extract-layers filename)
 
-  (display (string-append "Loading " filename "...")) (newline) (newline)
+  (gimp-message (string-append "Loading " filename "..."))
 
   (let* (
          (myimg (car (gimp-file-load RUN-NONINTERACTIVE filename filename)))
          (layer-count (car (gimp-image-get-layers myimg)))
+         (layer-ids (cadr (gimp-image-get-layers myimg)))
         )
 
+    (gimp-message (string-append "Found " (number->string layer-count) " layers."))
 
-    (gimp-message (string-append "Found " (number->string layer-count) " layers.")) (newline)
+    (let* ((current-layer-index 0))
+      (while (< current-layer-index layer-count)
+        (gimp-message (string-append "Layer " (number->string current-layer-index)))
+        (let* ((current-layer (vector-ref layer-ids current-layer-index))
+               (current-layer-name (gimp-item-get-name current-layer))
+               )
+          ;; (gimp-message (string-append "Hop " (number->string current-layer)))
+          ;; (gimp-message (string-append "Hip " (class-of current-layer-name)))
+          ;; (gimp-message (string-append "Hip " (number->string current-layer-name)))
+          ;; (gimp-message (string-append "Layer " current-layer-name))
+        )
+        (set! current-layer-index (+ current-layer-index 1))
+      )
+    )
+
     (gimp-image-delete myimg)
   )
 )
