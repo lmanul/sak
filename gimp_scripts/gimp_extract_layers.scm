2df6238205b7c4544cc9004596f38bb5af129602
Manu Cornet <m@ma.nu>
2020-08-20 15:23:44 +0000

    More GIMP Scheme experimenting

--- gimp_scripts/gimp_extract_layers.scm
+++ gimp_scripts/gimp_extract_layers.scm
@@ -1,5 +1,17 @@
 (define (boolean->string val) (if val "#t" "#f"))
 
+(define (gimp-image-save-with-only-layer in-layer img)
+  (let* (
+    (the-width (car (gimp-image-width img)))
+    (the-height (car (gimp-image-height img)))
+    (the-type (car (gimp-image-base-type img)))
+    (the-new-img (car (gimp-image-new the-width the-height the-type)))
+    )
+    ;; gimp-layer-new
+    ;; gimp-image-insert-layer
+  )
+)
+
 (define (show-only-layer in-layer img)
   (gimp-message (string-append "Hiding all layers but " (number->string in-layer)))
   (let* (
@@ -45,6 +57,8 @@
           (gimp-message (string-append "Layer number " (number->string i)))
           ;; (gimp-message (boolean->string (car (gimp-layer-is-floating-sel layer))))
           (show-only-layer layer img)
+          (gimp-image-merge-visible-layers img EXPAND-AS-NECESSARY)
+          (gimp-image-save-with-only-layer layer img)
         )
         (set! i (+ i 1))
       )
