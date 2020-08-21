3d2f8697e0c1e76c06577ba5c66bcc4d8778f0f4
Manu Cornet <m@ma.nu>
2020-08-21 22:37:10 +0000

    Okay, the Scheme code to export individual layers finally works

--- gimp_scripts/gimp_extract_layers.scm
+++ gimp_scripts/gimp_extract_layers.scm
@@ -1,38 +1,12 @@
-(define (boolean->string val) (if val "#t" "#f"))
-
-(define (gimp-image-save-with-only-layer in-layer img)
+(define (gimp-image-save-with-only-layer in-layer img in-filename)
   (let* (
     (the-width (car (gimp-image-width img)))
     (the-height (car (gimp-image-height img)))
     (the-type (car (gimp-image-base-type img)))
     (the-new-img (car (gimp-image-new the-width the-height the-type)))
+    (the-copied-layer (car (gimp-layer-new-from-drawable in-layer the-new-img)))
     )
-    ;; gimp-layer-new
-    ;; gimp-image-insert-layer
-  )
-)
-
-(define (show-only-layer in-layer img)
-  (gimp-message (string-append "Hiding all layers but " (number->string in-layer)))
-  (let* (
-         (layers (gimp-image-get-layers img))
-         (num-layers (car layers))
-         (layer-array (cadr layers))
-        ;; )
-    )
-    (let* (
-           (i 0)
-           )
-      (while (< i num-layers)
-        (let* (
-          (layer (aref layer-array i)))
-          (if (= layer in-layer)
-              (gimp-item-set-visible layer TRUE)
-              (gimp-item-set-visible layer FALSE))
-        )
-        (set! i (+ i 1))
-      )
-    )
+    (gimp-file-save RUN-NONINTERACTIVE the-new-img the-new-img in-filename in-filename)
   )
 )
 
@@ -45,6 +19,7 @@
          (layers (gimp-image-get-layers img))
          (num-layers (car layers))
          (layer-array (cadr layers))
+         (export-file-name-prefix (string-append (substring filename 0 (- (string-length filename) 4)) "_layer"))
         )
 
     (gimp-message (string-append "Found " (number->string num-layers) " layers."))
@@ -53,12 +28,10 @@
       (while (< i num-layers)
         (let* ((layer (aref layer-array i))
                (name (gimp-item-get-name layer))
+               (export-file-name (string-append export-file-name-prefix (number->string i) ".jpg"))
                )
           (gimp-message (string-append "Layer number " (number->string i)))
-          ;; (gimp-message (boolean->string (car (gimp-layer-is-floating-sel layer))))
-          (show-only-layer layer img)
-          (gimp-image-merge-visible-layers img EXPAND-AS-NECESSARY)
-          (gimp-image-save-with-only-layer layer img)
+          (gimp-image-save-with-only-layer layer img export-file-name)
         )
         (set! i (+ i 1))
       )
