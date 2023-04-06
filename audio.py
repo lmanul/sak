6b4d4fd553f65630ae9173a3422755a64d183d94
Manu Cornet <m@ma.nu>
2023-04-06 22:43:32 +0000

    Be case-insensitive when looking for dbus services for pausing

--- audio.py
+++ audio.py
@@ -8,7 +8,7 @@ def send_command_to_music_player(cmd):
 
     destination = ""
     for service in dbus.SessionBus().list_names():
-        if "vlc" in service or "lollypop" in service:
+        if "vlc" in service or "lollypop" in service.lower():
             destination = service
             # Target the first service we find
             break
