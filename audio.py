da57ec7226dbaf9fa3f5ac96c959060341092ba4
Manu Cornet <m@ma.nu>
2023-08-24 13:26:43 +0000

    Support more music players when sending play/pause signals

--- audio.py
+++ audio.py
@@ -4,14 +4,25 @@ import dbus
 
 PREFIX = "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player."
 
+MUSIC_PLAYERS = [
+  "clementine",
+  "lollypop",
+  "vlc",
+]
+
 def send_command_to_music_player(cmd):
 
     destination = ""
     for service in dbus.SessionBus().list_names():
-        if "vlc" in service or "lollypop" in service.lower():
-            destination = service
-            # Target the first service we find
-            break
+        found = False
+        for player in MUSIC_PLAYERS:
+            if player in service.lower():
+                # Target the first service we find
+                destination = service
+                found = True
+                break
+            if found:
+                break
 
     if destination:
         os.system("dbus-send --type=method_call --dest='" + destination + "' " + PREFIX + cmd)
