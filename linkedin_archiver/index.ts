0ca69d615964d7c88c8611d15e4a01cc2e0436c8
Manu Cornet <m@ma.nu>
2024-08-08 21:08:36 +0900

    First working version for 'linkedin archiver

--- linkedin_archiver/index.ts
+++ linkedin_archiver/index.ts
@@ -4,6 +4,12 @@ import { type Page } from 'puppeteer';
 const user = 'replaceme@example.com';
 const password = 'REPLACE-ME';
 
+const delay = (milliseconds: number) => {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, milliseconds)
+    });
+ }
+
 const login = async (page: Page) => {
     await page.waitForSelector('#username');
     await page.type('#username', user);
@@ -11,6 +17,29 @@ const login = async (page: Page) => {
     await page.type('#password', password);
     page.keyboard.press('Enter');
     await page.waitForNavigation();
+};
+
+// Returns whether we have actually archived a message.
+const archiveFirstShownMessage = async (page: Page): Promise<boolean> => {
+    let item;
+    try {
+        item = await page.waitForSelector('.msg-conversation-listitem__link', { timeout: 5000});
+    } catch (TimeoutError) {
+        // Looks like we're done.
+        return false;
+    }
+    await item?.click();
+    const dropdownButton = await page.waitForSelector(
+        '.msg-overlay-conversation-bubble .msg-thread-actions__dropdown');
+    await dropdownButton?.click();
+    try {
+      const archiveMenuItem = await page.waitForSelector('text/Archive');
+      await archiveMenuItem?.click();
+    } catch (TimeoutError) {
+      // Oh well. Skip this one.
+      return true;
+    }
+    return true;
 };
 
 const main = async () => {
@@ -23,6 +52,20 @@ const main = async () => {
     await page.setViewport({width: 1080, height: 1024});
     await page.goto('https://linkedin.com/login');
     await login(page);
+    let keepGoing = true;
+    while (keepGoing) {
+        keepGoing = await archiveFirstShownMessage(page);
+        await delay(300);
+    }
+    // There's also the "Other" tab.
+    keepGoing = true;
+    const otherTab = await page.waitForSelector('text/Other');
+    await otherTab?.click();
+    while (keepGoing) {
+        keepGoing = await archiveFirstShownMessage(page);
+        await delay(300);
+    }
+    browser.close();
 };
 
 main();
