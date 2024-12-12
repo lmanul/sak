30ebcd9ece9d28cbc44d547c54161b24fb6001c2
Manu Cornet <m@ma.nu>
2024-12-12 07:27:03 +0000

    Refactor some workspace switching into the window manager file

--- windowmanager.py
+++ windowmanager.py
@@ -2,8 +2,46 @@ import os
 import shlex
 import subprocess
 
-def get_focused_monitor():
+import monitors
+
+def is_bspwm():
     session = os.getenv("XDG_SESSION_DESKTOP")
-    if session == "bspwm":
+    return session == "bspwm"
+
+def get_focused_monitor():
+    if is_bspwm():
         return subprocess.check_output(shlex.split(
             "bspc query -M -d focused --names")).decode().strip()
+
+def get_monitor_ids():
+    if is_bspwm():
+        # Faster than using xrandr
+        return subprocess.check_output(shlex.split(
+            "bspc query -M --names")).decode().strip().split("\n")
+    connected_monitors = [m for m in monitors.get_monitors() if m.connected]
+    return [m.input_id for m in connected_monitors]
+
+def focus_monitor_with_id(monitor_id):
+    if is_bspwm():
+        os.system("bspc monitor -f " + monitor_id)
+
+def select_target_workspace(n_rows, n_cols, current, direction):
+    target = current
+    if direction == "l":
+        is_leftmost = (current % n_cols == 1)
+        if not is_leftmost:
+            target = current - 1
+    if direction == "r":
+        is_rightmost = (current % n_cols == 0)
+        if not is_rightmost:
+            target = current + 1
+    if direction == "u":
+        is_topmost = (current <= n_cols)
+        if not is_topmost:
+            target = current - n_cols
+    if direction == "d":
+        is_bottommost = (current > (n_rows - 1) * n_cols)
+        if not is_bottommost:
+            target = current + n_cols
+
+    return target
