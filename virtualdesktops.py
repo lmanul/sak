56499e4c690793b37d06f8b2077920677102b302
Manu Cornet <m@ma.nu>
2025-02-06 18:14:17 +0000

    Virtual desktops, support 4 monitors. Add Devanagari numerals

--- virtualdesktops.py
+++ virtualdesktops.py
@@ -1,10 +1,11 @@
 ARABIC = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 ROMAN  = "I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX"
 CHINESE = "一 二 三 四 五 六 七 八 九 十 十一 十二 十三 十四 十五 十六 十七 十八 十九 二十"
+DEVANAGARI = "१ २ ३ ४ ५ ६ ७ ८ ९ १० ११ १२ १३ १४ १५ १६ १७ १८ १९ २०"
 
-SETS = [ ARABIC, ROMAN, CHINESE ]
+SETS = [ ARABIC, ROMAN, CHINESE, DEVANAGARI ]
 
-ALL = [ARABIC, ROMAN, CHINESE]
+ALL = [ARABIC, ROMAN, CHINESE, DEVANAGARI]
 
 # Takes a 1-based index
 def name_from_index(index, n_rows, n_cols):
