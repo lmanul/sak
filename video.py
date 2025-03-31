7fd30424b3aeb316c991d32b1db1612d6713ab06
Manu Cornet <m@ma.nu>
2025-03-31 09:22:26 +0000

    Add a script to list all audio tracks in a nice to read format

---
+++ video.py
@@ -0,0 +1,15 @@
+import json
+import os
+import shlex
+import subprocess
+
+def get_mkv_audio_tracks(mkv):
+    retval = {}
+    j = subprocess.check_output(shlex.split("mkvmerge -J " + mkv)).decode()
+    for track in json.loads(j)['tracks']:
+        if track["type"] == "audio":
+            lang_id = track["properties"]["language"]
+            retval[lang_id] = {}
+            retval[lang_id]["number"] = track["properties"]["number"]
+            retval[lang_id]["name"] = track["properties"]["track_name"]
+    return retval
