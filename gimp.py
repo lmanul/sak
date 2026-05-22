83cdcee54f7a9a6998ed335ef73e3829d628f525
Manu Cornet <m@ma.nu>
2026-05-22 08:31:08 +0000

    Update some links from GIMP 3.0 to 3.2

--- gimp.py
+++ gimp.py
@@ -2,7 +2,7 @@ import os
 
 def run_script(script_code, function_to_run):
     cmd = (
-        "/usr/bin/gimp-3.0 -i --batch-interpreter=plug-in-script-fu-eval -b - << EOF"
+        "/usr/bin/gimp-3.2 -i --batch-interpreter=plug-in-script-fu-eval -b - << EOF"
         "" + script_code + " "
         "" + function_to_run + " "
         "(gimp-quit 0))"
@@ -12,7 +12,7 @@ def run_script(script_code, function_to_run):
 
 def run_script_python(script_code, function_to_run):
     cmd = (
-        "/usr/bin/gimp-3.0 -i --batch-interpreter=python-fu-eval "
+        "/usr/bin/gimp-3.2 -i --batch-interpreter=python-fu-eval "
         "-b "
         "\"" + script_code + "" + function_to_run + ";\" "
         "--quit"
