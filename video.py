ccc3c1a85f7e5f85d4378b297cc8916deee699f3
Manu Cornet <m@ma.nu>
2026-09-18 20:15:12 +0900

    Add 'videolistsubtitletracks'

--- video.py
+++ video.py
@@ -8,9 +8,21 @@ def get_mkv_audio_tracks(mkv):
     j = subprocess.check_output(shlex.split("mkvmerge -J " + mkv)).decode()
     for track in json.loads(j)['tracks']:
         if track["type"] == "audio":
-            lang_id = track["properties"]["language"]
-            retval[lang_id] = {}
-            retval[lang_id]["number"] = track["properties"]["number"]
+            number = track["properties"]["number"]
+            retval[number] = {}
+            retval[number]["language"] = track["properties"]["language"]
             if "track_name" in track["properties"]:
-                retval[lang_id]["name"] = track["properties"]["track_name"]
+                retval[number]["name"] = track["properties"]["track_name"]
+    return retval
+
+def get_mkv_subtitle_tracks(mkv):
+    retval = {}
+    j = subprocess.check_output(shlex.split("mkvmerge -J " + mkv)).decode()
+    for track in json.loads(j)['tracks']:
+        if track["type"] == "subtitles":
+            number = track["properties"]["number"]
+            retval[number] = {}
+            retval[number]["language"] = track["properties"]["language"]
+            if "track_name" in track["properties"]:
+                retval[number]["name"] = track["properties"]["track_name"]
     return retval
